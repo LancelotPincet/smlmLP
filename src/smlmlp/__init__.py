cdb08e968bffa0c027d58c155471438ed1da4108
LancelotPincet <lancelot.pincet@free.fr>
2026-01-23 23:52:36 +0100

    auto-commit

--- src/smlmlp/__init__.py
+++ src/smlmlp/__init__.py
@@ -23,6 +23,8 @@ from contextlib import contextmanager
 def resources_dir():
     with resources.as_file(resources.files("smlmlp.resources")) as path:
         yield path
+if False: 
+    import smlmlp.resources
 
 # %% Hidden imports
 if False :
