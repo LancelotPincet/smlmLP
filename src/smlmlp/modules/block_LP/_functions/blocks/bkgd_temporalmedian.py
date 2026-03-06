35bbae8c698f994514a61b29061535a9d47ed184
LancelotPincet <lancelot.pincet@free.fr>
2026-03-06 19:39:59 +0100

    auto-commit

--- src/smlmlp/modules/block_LP/_functions/blocks/bkgd_temporalmedian.py
+++ src/smlmlp/modules/block_LP/_functions/blocks/bkgd_temporalmedian.py
@@ -8,14 +8,26 @@
 # %% Libraries
 from smlmlp import block
 import numpy as np
+from stacklp import temporal_median
 
 
 
 # %% Function
 @block()
-def bkgd_temporalmedian(*channels, iterator=range) :
+def bkgd_temporalmedian(*channels, temporal_window=25, pad=0, bkgds=None, cuda=False, parallel=False) :
     '''
-    TODO.
+    This function creates the temporal median background.
     '''
 
-    return
+    # Correct bkgd length for end of acquisition
+    if bkgds is not None :
+        bkgds = [bkgd[:len(channel) - pad] for channel, bkgd in zip(channels, bkgds)]
+    
+    new_bkgds = []
+    for i in range(len(channels)) :
+        channel = channels[i]
+        bkgd = None if bkgds is None else bkgds[i]
+        new_bkgd = temporal_median(channel, temporal_window, out=bkgd, pad=pad, cuda=cuda, parallel=parallel)
+        new_bkgds.append(new_bkgd)
+
+    return new_bkgds
