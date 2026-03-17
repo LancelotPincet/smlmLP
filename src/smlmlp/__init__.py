658171e6544fea24737981b791c75b912d073b10
LancelotPincet <lancelot.pincet@free.fr>
2026-03-17 05:01:04 +0100

    auto-commit

--- src/smlmlp/__init__.py
+++ src/smlmlp/__init__.py
@@ -19,23 +19,24 @@ sources = {
 'columns': 'smlmlp.modules.columns_LP.columns',
 'computer': 'smlmlp.modules.computer_LP.computer',
 'dataframes': 'smlmlp.modules.dataframes_LP.dataframes',
+'Camera': 'smlmlp.modules.Config_LP._functions.Camera',
 'metadatum': 'smlmlp.modules.Config_LP._functions.metadatum',
 'Channel': 'smlmlp.modules.Config_LP._functions.Channel',
-'Camera': 'smlmlp.modules.Config_LP._functions.Camera',
 'save_df': 'smlmlp.modules.Locs_LP._functions.save_df',
 'open_df': 'smlmlp.modules.Locs_LP._functions.open_df',
 'load_data': 'smlmlp.modules.block_LP._functions.blocks.load.load_data',
-'bkgd_spatialmean': 'smlmlp.modules.block_LP._functions.blocks.bkgd.bkgd_spatialmean',
 'bkgd_spatialmini': 'smlmlp.modules.block_LP._functions.blocks.bkgd.bkgd_spatialmini',
 'bkgd_temporalmedian': 'smlmlp.modules.block_LP._functions.blocks.bkgd.bkgd_temporalmedian',
-'signal_dog': 'smlmlp.modules.block_LP._functions.blocks.signal.signal_dog',
+'bkgd_spatialmean': 'smlmlp.modules.block_LP._functions.blocks.bkgd.bkgd_spatialmean',
+'signal_temporalfilter': 'smlmlp.modules.block_LP._functions.blocks.signal.signal_temporalfilter',
+'signal_spatialfilter': 'smlmlp.modules.block_LP._functions.blocks.signal.signal_spatialfilter',
 'blink_psf': 'smlmlp.modules.block_LP._functions.blocks.blink.blink_psf',
 'blink_on': 'smlmlp.modules.block_LP._functions.blocks.blink.blink_on',
 'column': 'smlmlp.modules.columns_LP._functions.column',
 'LocsDataFrame': 'smlmlp.modules.dataframes_LP._functions.LocsDataFrame',
+'MainDataFrame': 'smlmlp.modules.dataframes_LP._functions.MainDataFrame',
 'DetsDataFrame': 'smlmlp.modules.dataframes_LP._functions.DetsDataFrame',
-'DataFrame': 'smlmlp.modules.dataframes_LP._functions.DataFrame',
-'MainDataFrame': 'smlmlp.modules.dataframes_LP._functions.MainDataFrame'
+'DataFrame': 'smlmlp.modules.dataframes_LP._functions.DataFrame'
 }
 
 
